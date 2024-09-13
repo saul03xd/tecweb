d812f984b8c3f932b69a9cce397893c2741313a7
saul03xd <saul.tl0307@gmail.com>
2024-09-12 19:49:12 -0600

    Ejercio 1 resuelto

---
+++ practicas/P07/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 4</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <?php
+        include 'src/funciones.php';
+        esMultiploDe5Y7($_GET['numero']);
+    ?>
+</body>
+</html>
